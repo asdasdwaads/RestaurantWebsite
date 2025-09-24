64e59b71c4e140547e68da7a15a639cf8f249a1f
porto <yospunyaporto@gmail.com>
2025-09-24 12:11:57 +0700

    hotfix(image): remove import Image]

--- src/components/SliderImage.tsx
+++ src/components/SliderImage.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { motion } from "motion/react";
-import Image from "next/image";
 import { useState, useEffect } from "react";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
